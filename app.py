5ff116646c6e309ccee30ea41be6f22de75311b8
Louis Pluviose <louis.pluviose@uha.fr>
2023-10-02 10:21:39 +0200

    build: connection to database from the app
    The app can now connect/disconnect to the database via buttons

--- app.py
+++ app.py
@@ -11,16 +11,41 @@ class MyWidget(QtWidgets.QWidget):
         super().__init__()
 
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-        
-        self.button = QtWidgets.QPushButton("Connect to database")
-        if db.DatabaseConnection().connection.is_connected():
-            self.button.setText("Connected to database")
+
+        # Créez une instance de DatabaseConnection
+        self.db = db.DatabaseConnection()
+
+        self.text = QtWidgets.QLabel("")
+
+        # Bouton pour se connecter à la base de données
+        self.connect_button = QtWidgets.QPushButton("Connect to database")
+        self.connect_button.clicked.connect(self.connect_to_database)
+
+        # Bouton pour se déconnecter de la base de données
+        self.disconnect_button = QtWidgets.QPushButton("Disconnect from database")
+        self.disconnect_button.clicked.connect(self.disconnect_from_database)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.connect_button)
+        self.layout.addWidget(self.disconnect_button)
 
-        self.button.clicked.connect(self.magic)
+    @QtCore.Slot()
+    def connect_to_database(self):
+        # Essayez de vous connecter à la base de données
+        self.db.connect()
+        
+        # Vérifiez si la connexion est établie
+        if self.db.connection.is_connected():
+            self.connect_button.setText("Connected to database")
+        else:
+            self.connect_button.setText("Failed to connect")
+
+    @QtCore.Slot()
+    def disconnect_from_database(self):
+        # Essayez de vous déconnecter de la base de données
+        self.db.disconnect()
+        self.connect_button.setText("Connect to database")
 
     @QtCore.Slot()
     def magic(self):
@@ -34,6 +59,3 @@ if __name__ == "__main__":
     widget.show()
 
     sys.exit(app.exec())
-
-class Test:
-    print("Debug mode enabled")
