323d171566a20de30438f49ae9c908c850553abe
Louis Pluviose <louis.pluviose@uha.fr>
2023-10-03 09:45:27 +0200

    test: testing queries to the database

--- database_connection.py
+++ database_connection.py
@@ -1,7 +1,6 @@
 import mysql.connector as mysql
 from mysql.connector import Error
 
-
 class DatabaseConnection:
     def __init__(self):
         self.host = '45.154.99.10'
@@ -18,9 +17,10 @@ class DatabaseConnection:
                 user=self.user,
                 password=self.password
             )
-            print('MySQL Database connection successful')
+            if self.connection.is_connected():
+                print('Connected to MySQL database')
         except Error as e:
-            print(f'The error "{e}" occurred')
+            print(f'Error while connecting to MySQL: {e}')
     
     def disconnect(self):
         if self.connection.is_connected():
@@ -31,8 +31,10 @@ class DatabaseConnection:
         try:
             cursor = self.connection.cursor()
             cursor.execute(query)
-            self.connection.commit()
-            print('Query executed successfully')
+            result = cursor.fetchall()
+            print(f'Query: {query}')
+            print(f'Result: {result}')
+            print()
         except Error as e:
             print(f'The error "{e}" occurred')
 
@@ -40,4 +42,15 @@ class main:
     def __init__(self):
         self.db = DatabaseConnection()
         self.db.connect()
+        #self.db.execute_query('SELECT * FROM USER')
+        #self.db.execute_query('SELECT * FROM TASK')
+        #self.db.execute_query('SELECT * FROM SUBTASK')
+        self.db.execute_query('SELECT * FROM USER')
+        self.db.execute_query('INSERT INTO USER (mail, password, name, rights_id) VALUES ("test@test.com", "testpassword", "testname",2)')
+        self.db.execute_query('SELECT * FROM USER')
+        #self.db.execute_query('INSERT INTO FLAG (name, color) VALUES ("testflagviapython", "#000000")')
+        #self.db.execute_query('SELECT * FROM FLAG')
         self.db.disconnect()
+
+if __name__ == '__main__':
+    main()
