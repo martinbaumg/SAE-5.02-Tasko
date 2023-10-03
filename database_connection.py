c87775ea30a3442032bc882ea2b89702c67f28b0
Louis Pluviose <louis.pluviose@uha.fr>
2023-10-03 11:37:49 +0200

    test: full test for the database

--- database_connection.py
+++ database_connection.py
@@ -1,56 +1,62 @@
-import mysql.connector as mysql
-from mysql.connector import Error
+import mysql.connector as mysql # On importe le module mysql.connector
+from mysql.connector import Error # On importe le module Error
 
-class DatabaseConnection:
-    def __init__(self):
+class DatabaseConnection: # On créé une classe DatabaseConnection
+    def __init__(self): # On créé une méthode __init__ qui sera appelée à chaque fois qu'on créé une instance de DatabaseConnection
         self.host = '45.154.99.10'
         self.database = 'tasko'
         self.user = 'admin'
         self.password = '48wGK#t0:ZOx9c'
         self.connection = None
     
-    def connect(self):
+    def connect(self): # On créé une méthode connect qui va nous permettre de nous connecter à la BDD
         try:
-            self.connection = mysql.connect(
+            self.connection = mysql.connect( # On se connecte à la BDD
                 host=self.host,
                 database=self.database,
                 user=self.user,
                 password=self.password
             )
-            if self.connection.is_connected():
+            if self.connection.is_connected(): # Si on est connecté à la BDD, on affiche un message
                 print('Connected to MySQL database')
-        except Error as e:
+                return self.connection
+        except Error as e: # Si on a une erreur, on l'affiche
             print(f'Error while connecting to MySQL: {e}')
     
-    def disconnect(self):
+    def disconnect(self): # On créé une méthode disconnect qui va nous permettre de nous déconnecter de la BDD
         if self.connection.is_connected():
             self.connection.close()
             print('MySQL Database connection closed')
 
-    def execute_query(self, query):
+    def execute_query(self, query): # On créé une méthode execute_query qui va nous permettre d'exécuter une requête SQL
         try:
-            cursor = self.connection.cursor()
-            cursor.execute(query)
-            result = cursor.fetchall()
-            print(f'Query: {query}')
-            print(f'Result: {result}')
-            print()
-        except Error as e:
+            cursor = self.connection.cursor() # On créé un curseur
+            cursor.execute(query) # On exécute la requête
+            result = cursor.fetchall() # On récupère le résultat de la requête
+            print(f'Query: {query}') # On affiche la requête
+            print(f'Result: {result}') # On affiche le résultat
+            print() # On affiche un saut de ligne
+        except Error as e: # Si on a une erreur, on l'affiche
             print(f'The error "{e}" occurred')
 
-class main:
+class main: # On créé une classe main
     def __init__(self):
-        self.db = DatabaseConnection()
-        self.db.connect()
-        #self.db.execute_query('SELECT * FROM USER')
-        #self.db.execute_query('SELECT * FROM TASK')
-        #self.db.execute_query('SELECT * FROM SUBTASK')
-        self.db.execute_query('SELECT * FROM USER')
-        self.db.execute_query('INSERT INTO USER (mail, password, name, rights_id) VALUES ("test@test.com", "testpassword", "testname",2)')
-        self.db.execute_query('SELECT * FROM USER')
-        #self.db.execute_query('INSERT INTO FLAG (name, color) VALUES ("testflagviapython", "#000000")')
-        #self.db.execute_query('SELECT * FROM FLAG')
-        self.db.disconnect()
+        self.db = DatabaseConnection() # On créé une instance de DatabaseConnection
+        self.connection = self.db.connect() # On se connecte à la BDD
 
-if __name__ == '__main__':
+
+        #self.db.execute_query('SELECT SUBTASK.name, USER.name FROM SUBTASK_USER INNER JOIN SUBTASK ON SUBTASK_USER.subtask_id = SUBTASK.id INNER JOIN USER ON SUBTASK_USER.user_id = USER.id;') # Requête pour les utilisateurs affectés à une sous-tâche
+        #self.db.execute_query('SELECT TASK.name, SUBTASK.name FROM SUBTASK_USER INNER JOIN SUBTASK ON SUBTASK_USER.subtask_id = SUBTASK.id INNER JOIN TASK_SUBTASK ON SUBTASK.id = TASK_SUBTASK.subtask_id INNER JOIN TASK ON TASK_SUBTASK.task_id = TASK.id;') # Requête pour les sous-tâches affectées à une tâche
+        #self.db.execute_query('SELECT TASK.name, USER.name FROM TASK_USER INNER JOIN TASK ON TASK_USER.task_id = TASK.id INNER JOIN USER ON TASK_USER.user_id = USER.id;') # Requête pour les utilisateurs affectés à une tâche
+        
+
+        #self.db.execute_query('INSERT INTO USER (mail, password, name, rights_id) VALUES ("micha@test.com", "micha", "micha",2)') # Ajout d'un utilisateur à la BDD
+        #self.db.execute_query('INSERT INTO TASK (name, description, priority_id, flag_id, user_id, state_id, subtask_id) VALUES ("testviapython", "descriptionviapython", 1, 1, 1, 1, 1)') # Ajout d'une tâche à la BDD
+        #self.db.execute_query('INSERT INTO FLAG (name, color) VALUES ("testviapython", "#000000")') # Ajout d'un flag à la BDD
+
+        #self.connection.commit() # On applique les changements à la BDD
+
+        self.db.disconnect() # On se déconnecte de la BDD
+
+if __name__ == '__main__': # Si on lance le fichier database_connection.py, on exécute la méthode main
     main()
